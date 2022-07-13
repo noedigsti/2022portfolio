454c85f8f9533db5e3cb09409ee91c6cb9e7d750
Minh <noedigsti@gmail.com>
2022-07-13 15:59:44 +1000

    fix

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
         <p className="mb-4 text-gray-600 dark:text-gray-400">
           A collection of projects I&apos;ve worked on.
         </p>
-        <a href={'https://github.com/noedigsti/My-Community-Landmark'}className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900">
+        <a href={'https://github.com/noedigsti/My-Community-Landmark'} className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900">
           <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
             Landmark
           </h3>
