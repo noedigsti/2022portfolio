6a7f2e0b356359ff781c44cb3dd3be61a9843423
noedigsti <noedigsti@gmail.com>
2022-08-25 00:47:55 +1000

    update

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -6,7 +6,10 @@ export default function Resume() {
   return (
     <Container title="Resume – Cuong Minh Vu">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <a href={'/[UPDATED] Cuong Minh Vu Resume.pdf'} className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900">
+        <a
+          href={'/Cuong Minh Vu Resume.pdf'}
+          className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900"
+        >
           <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
             Click to view my Resume!
           </h3>
